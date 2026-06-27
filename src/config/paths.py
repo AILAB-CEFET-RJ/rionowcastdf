a501535c466194f6259a935f007e509d68d9394b
Vinicius Gonçalves <viniciusgsantos@poli.ufrj.br>
2026-06-27 14:24:17 -0300

    gerou radar_cache

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 
 
-ERA5_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/reanalysis/cds/era5/pressure"
-RADAR_CACHE_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/radar_cache"
-RADAR_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/radar_sumare"
+ERA5_DIR= "/home/vsantos/rionowcast/data/reanalysis/cds/era5/pressure"
+RADAR_CACHE_DIR = "/home/vsantos/rionowcast/data/radar_cache"
+RADAR_DIR = "/home/vsantos/rionowcast/data/radar_sumare"
+
+#ERA5_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/reanalysis/cds/era5/pressure"
+#RADAR_CACHE_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/radar_cache"
+#RADAR_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/radar_sumare"
+
+
