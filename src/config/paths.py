809d8910ec265df175a090c242d03dccdbea13b7
Vinicius Goncalves <viniciusgsantos@poli.ufrj.br>
2026-08-10 10:10:38 -0300

    ajustar era5_dir

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 
-ERA5_DIR= "/home/vsantos/rionowcast/data/reanalysis/cds/era5/pressure"
+ERA5_DIR= "/home/vsantos/rionowcast/data/reanalysis/cds/era5"
 RADAR_CACHE_DIR = "/home/vsantos/rionowcast/data/radar_cache"
 RADAR_DIR = "/home/vsantos/rionowcast/data/radar_sumare"
+
 
 #ERA5_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/reanalysis/cds/era5/pressure"
 #RADAR_CACHE_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/radar_cache"
