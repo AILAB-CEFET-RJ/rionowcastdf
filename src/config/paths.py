019929bf220ab6ca2bdd12107dab3e4c402ee2ae
Vinicius Goncalves <viniciusgsantos@poli.ufrj.br>
2026-09-01 22:25:04 -0300

    alterar os paths no arquivo de configs

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 
-ERA5_DIR= "/home/vsantos/rionowcast/data/reanalysis/cds/era5"
-RADAR_CACHE_DIR = "/home/vsantos/rionowcast/data/radar_cache"
-RADAR_DIR = "/home/vsantos/rionowcast/data/radar_sumare"
+ERA5_DIR= "/home/vsantos/data/reanalysis/cds/era5"
+RADAR_CACHE_DIR = "/home/vsantos/data/radar_cache"
+RADAR_DIR = "/home/vsantos/data/radar_sumare"
 
 
 #ERA5_DIR = "/home/sangonvi/Cefet/repositories/rionowcastdf/data/reanalysis/cds/era5/pressure"
